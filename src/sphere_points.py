d532f679a23d7c6f592c7ca067227158748c4f1e
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-10-15 17:02:45 +0200

    Refactoring

--- src/sphere_points.py
+++ src/sphere_points.py
@@ -7,11 +7,6 @@ import matplotlib.pyplot as plt
 from sympy import symbols, solvers
 from mpl_toolkits.mplot3d import Axes3D
 
-r = 5
-x, y, z = symbols('x y z', real=True)  # ONLY REAL values
-
-circle_equation = lambda P, r: (x - P[0]) ** 2 + (y - P[1]) ** 2 + (z - P[2]) ** 2 - r
-
 
 def get_solutions(P, r, n=100):
     """
@@ -23,6 +18,8 @@ def get_solutions(P, r, n=100):
     """
     result = []
     x0, y0, z0 = P
+    x, y, z = symbols('x y z', real=True)  # ONLY REAL values
+    circle_equation = lambda P, r: (x - P[0]) ** 2 + (y - P[1]) ** 2 + (z - P[2]) ** 2 - r
     equation = circle_equation((x0, y0, z0), r)
     while len(result) < n:
         x_, y_ = random.uniform(x0 - r, x0 + r), random.uniform(y0 - r, y0 + r)
@@ -34,27 +31,26 @@ def get_solutions(P, r, n=100):
     return result
 
 
-solutions1 = get_solutions((0., 0., 0.), 5)
-solutions2 = get_solutions((10., 10., 10.), 10)
+def plot_solutions(solutions_list: list):
+    def plotting_points(solutions, color, marker='o'):
+        ax.scatter(
+            [round(x[0], 2) for x in solutions],
+            [round(x[1], 2) for x in solutions],
+            [round(x[2], 2) for x in solutions],
+            c=color, marker=marker)
 
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
 
-def plotting_points(solutions, color, marker='o'):
-    ax.scatter(
-        [round(x[0], 2) for x in solutions],
-        [round(x[1], 2) for x in solutions],
-        [round(x[2], 2) for x in solutions],
-        c=color, marker=marker)
+    plotting_points(solutions_list[0], 'r')
+    plotting_points(solutions_list[1], 'b')
+    plt.show()
 
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
-
-plotting_points(solutions1, 'r')
-plotting_points(solutions2, 'b')
-plt.show()
 
 if __name__ == '__main__':
-    pass
+    solutions1 = get_solutions((0., 0., 0.), 5)
+    solutions2 = get_solutions((10., 10., 10.), 10)
+    plot_solutions([solutions1, solutions2])
